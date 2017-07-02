506800f603def7962ebaf003c2feae8b0e798ca8
mczernek <mczernek@redhat.com>
2017-07-02 18:05:52 +0200

    Remove old comments

--- src/main/java/cz/endhalf/Servlet.java
+++ src/main/java/cz/endhalf/Servlet.java
@@ -12,24 +12,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//Must be named same as from web.xml; otherwise, its name is FQCN; 2 names mapped to one context is not allowed
 @WebServlet(urlPatterns = {"/home"}, initParams=@WebInitParam(name="ProductName",value="DefaultValue"))
 public class Servlet extends HttpServlet {
     String appName;
 
     @Override
     public void init() throws ServletException {
-        // Uncomment to get init parameter from web.xml OR to get the default value defined in initParams
-        //appName=getInitParameter("ProductName");
-        //Uncomment to get
         appName = getServletContext().getInitParameter("ProductName");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //Change the response
         RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/WEB-INF/authenticate.jsp");
-
         requestDispatcher.forward(req,resp);
 
     }
@@ -46,6 +40,7 @@ public class Servlet extends HttpServlet {
         }
         if(p.isAuth()){
             RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/WEB-INF/app/auth.jsp");
+            //Sending the user as request-scoped; useless in real world
             req.setAttribute("user",p);
             requestDispatcher.forward(req,resp);
         }
